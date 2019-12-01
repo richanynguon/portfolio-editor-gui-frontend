c14d4c81ae08c3e2110512f8ee0c27bb8357ecb1
richanynguon <alina.nguon@gmail.com>
2019-12-01 02:16:08 -0500

    add types react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import { ApolloProvider } from "@apollo/react-hooks";
 import App from './App'
 import  client  from './ApolloProvider'
+import { BrowserRouter } from "react-router-dom";
+
 
 
 ReactDOM.render(
