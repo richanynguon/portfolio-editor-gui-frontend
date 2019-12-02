fc76c2e7ca3a70a1f97c07ed81735bc8465fca3b
richanynguon <alina.nguon@gmail.com>
2019-12-02 09:00:23 -0500

    commit before pr

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import * as Yup from 'yup';
 import { withFormik, FormikProps, ErrorMessage, Form, Field } from 'formik';
-import * as styled from 'styled-components'
+import * as s from '../../styles/styles'
+
 
 interface FormValues {
   user_name: string;
@@ -9,20 +10,25 @@ interface FormValues {
   password: string;
 }
 
-const InnerForm = (props:  FormikProps<FormValues>) => {
-  const { isSubmitting} = props;
+const InnerForm = (props: FormikProps<FormValues>) => {
+  const { isSubmitting } = props;
   return (
     <Form>
-      Username:
+      <s.Label>
+        Username:
       <Field type="name" name="user_name" />
-      <ErrorMessage name='name' />
-      Email:
+        <ErrorMessage name='name' />
+      </s.Label>
+      <s.Label>
+        Email:
       <Field type="email" name="email" />
-      <ErrorMessage name='email' />
-      Password:
+        <ErrorMessage name='email' />
+      </s.Label>
+      <s.Label>
+        Password:
       <Field type="password" name="password" />
-      <ErrorMessage name='password' />
-
+        <ErrorMessage name='password' />
+      </s.Label>
       <button type="submit" disabled={isSubmitting}>
         Submit
       </button>
@@ -44,24 +50,25 @@ const RegisterForm = withFormik<{}, FormValues>({
 
   validationSchema: Yup.object().shape({
     user_name: Yup.string()
-    .min(5, "Username must be 5 characters or longer")
-    .required("Username is required"),
-		email: Yup.string()
-			.email("Email not valid")
-			.required("Email is required"),
-		password: Yup.string()
-			.min(8, "Password must be 8 characters or longer")
-			.required("Password is required"),
-	}),
+      .min(5, "Username must be 5 characters or longer")
+      .required("Username is required"),
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be 8 characters or longer")
+      .required("Password is required"),
+  }),
 
   handleSubmit: (values, { setStatus, resetForm, setErrors, setSubmitting }) => {
-  
+
   },
 })(InnerForm);
 
 
 const Register = () => (
   <div>
+    Register
     <RegisterForm />
   </div>
 );
