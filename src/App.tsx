1365987fc606dce581ef7b1106c813671e1d1aeb
richanynguon <alina.nguon@gmail.com>
2020-01-13 17:46:40 -0500

    user element.hidden attribute

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC = () => {
     const imgElement = document.getElementById(`${30 - imagePos}`) as HTMLImageElement
     if (imagePos < 30) {
       if (imgElement) {
-        if (imgElement.hidden === true) {
+        if (imgElement.style.visibility === null || imgElement.style.visibility === 'visible') {
           imgElement.classList.remove('hidden');
         } else {
           imgElement.classList.add('hidden');
